a96256d1c6205d0dece5803a43587c0b969794cf
Anshika Gautam <anshikagautam130@gmail.com>
2022-01-12 23:02:51 +0530

    personal website

---
+++ index.js
@@ -0,0 +1,46 @@
+class TypeWriter {
+  constructor(txtEle, words, wait = 3000) {
+    this.txtEle = txtEle;
+    this.words = words;
+    this.txt = "";
+    this.wordIdx = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    const curr = this.wordIdx % this.words.length;
+    const fullWord = this.words[curr];
+    //check if deleting
+    if (this.isDeleting) {
+      this.txt = fullWord.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullWord.substring(0, this.txt.length + 1);
+    }
+    this.txtEle.innerHTML = `<span class="word">${this.txt}</span><span class="typing"></span>`;
+    let typeSpeed = 300;
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+    if (!this.isDeleting && this.txt === fullWord) {
+      typeSpeed = this.wait;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.wordIdx++;
+      typeSpeed = 500;
+    }
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+//init on DOM load
+document.addEventListener("DOMContentLoaded", init);
+
+//init App
+function init() {
+  const txtEle = document.querySelector(".typewriter");
+  const words = JSON.parse(txtEle.getAttribute("data-words"));
+  const wait = txtEle.getAttribute("data-wait");
+  new TypeWriter(txtEle, words, wait);
+}
